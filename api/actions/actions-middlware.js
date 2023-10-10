5efaa48d9bad21278428e985736fd23ccda6fb13
DJ1 <jimmynoble604@gmail.com>
2023-10-10 06:46:40 -0400

    actions-miuddleware.js

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,54 @@
-// add middlewares here related to actions
+const Actions = require('./actions-model')
+const Project = require('../projects/projects-model')
+
+async function checkActionId(req, res, next) {
+    try {
+        const action = await Actions.get(req.params.id);
+        if(action) {
+            req.action = action;
+            next();
+        }
+    } else {
+        next({ status: 404, message: `Action ${req.params.id} not found`})
+    }
+} catch(err) {
+    next(error)
+}
+
+async function checkNewAction(req, res, next) {
+    const { desciption, notes } = req.body;
+    if(desciption !== undefined &&
+        typeof desciption === 'string' && 
+        desciption.length && 
+        desciption.trim().length && 
+        desciption.length < 129 &&
+        notes !== undefined && 
+        notes.length && 
+        notes.trim().length) {
+            next()
+        } else {
+            res.status(400).json({
+                message: 'Action needs a name, valid project id and desciption',
+            })
+        }
+}
+
+async function checkValidProject(req, res, next) {
+    const { project_id } = req.body
+    const validProject = await Project.get(project_id)
+      try {
+        if(validProject) {
+            next();
+        } else {
+            next({ status: 404, message: `Project ${project_id} not found`})
+        } 
+      } catch (error) {
+        next(error)
+      }
+}
+
+module.exports = {
+    checkActionId,
+    checkNewAction,
+    checkValidProject
+}
